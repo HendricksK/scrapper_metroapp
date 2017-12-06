3f826dba52e55a83c1b5c7309ae39eddce92e08b
kurvin <kurvinhendricks@gmail.com>
2017-12-06 22:09:16 +0200

    Adding code to scrape site, getting downloadable links, will grab the appropriate links based on stringcompare, then pass that to the DB, and then start parsing to json

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app     = express();
+var express = require('express')
+var fs = require('fs')
+var request = require('request')
+var cheerio = require('cheerio')
+var app     = express()
 
 app.get('/scrape', function(req, res){
 
-  //All the web scraping magic will happen here
+	url = 'http://capetowntrains.freeblog.site/timetables'
+
+	request(url, function(error, response, html) {
+		if(!error) {
+
+			var $ = cheerio.load(html)
+
+			$('span a').filter(function() {
+				var data = $(this)
+				console.log(data.text())
+				console.log(data.attr('href'))
+			})
+		}
+	})
 
 })
 
 app.listen('8081')
 
-console.log('Magic happens on port 8081');
-
-exports = module.exports = app;
+console.log('Magic happens on port 8081')
+exports = module.exports = app
